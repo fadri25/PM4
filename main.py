3823bdd4c71004a0937e5a20aa6a99e7dcf8b58b
fadri25 <75944399+fadri25@users.noreply.github.com>
2025-03-21 09:59:02 +0100

    fadridev erweitert

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from getdata import CSVLoader, CSVSampler
 from data_transformation import DataTransformation, CustomerFeatures, Terminalriskfeatures
+import xgboost
+import pandas as pd
+import sklearn
+import numpy as np
+import matplotlib as plt
 
 class Gui:
     def testgui():
         print("test")
 
 if __name__ == "__main__":
-    loader = CSVLoader(f"C:/PM4/transactions.csv")
-    loader.load_csv()
+    #loader = CSVLoader(f"C:/PM4/transactions.csv")
+    #loader.load_csv()
     begin_date = "2018-04-01"
     end_date = "2019-09-30"
     input_file = "C:/PM4/transactions.csv"
